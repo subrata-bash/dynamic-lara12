b822af67e763bffbd3c0c8f41a3889d4d1074dc9
Subrata Ghosh Dastidar <github.subrata@gmail.com>
2025-06-08 02:20:42 +0530

    admin page single dashboard add

--- resources/views/admin/body/sidebar.blade.php
+++ resources/views/admin/body/sidebar.blade.php
@@ -7,18 +7,18 @@
                         <div class="logo-box">
                             <a href="index.html" class="logo logo-light">
                                 <span class="logo-sm">
-                                    <img src="assets/images/logo-sm.png" alt="" height="22">
+                                    <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="" height="22">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="assets/images/logo-light.png" alt="" height="24">
+                                    <img src="{{ asset('backend/assets/images/logo-light.png') }}" alt="" height="24">
                                 </span>
                             </a>
                             <a href="index.html" class="logo logo-dark">
                                 <span class="logo-sm">
-                                    <img src="assets/images/logo-sm.png" alt="" height="22">
+                                    <img src="{{ asset('backend/assets/images/logo-sm.png') }}" alt="" height="22">
                                 </span>
                                 <span class="logo-lg">
-                                    <img src="assets/images/logo-dark.png" alt="" height="24">
+                                    <img src="{{ asset('backend/assets/images/logo-dark.png') }}" alt="" height="24">
                                 </span>
                             </a>
                         </div>
@@ -26,25 +26,12 @@
                         <ul id="side-menu">
 
                             <li class="menu-title">Menu</li>
-
                             <li>
-                                <a href="#sidebarDashboards" data-bs-toggle="collapse">
+                                <a href="{{ route('dashboard') }}" class="tp-link">
                                     <i data-feather="home"></i>
-                                    <span> Dashboard </span>
-                                    <span class="menu-arrow"></span>
+                                    <span>Dashboard</span>
                                 </a>
-                                <div class="collapse" id="sidebarDashboards">
-                                    <ul class="nav-second-level">
-                                        <li>
-                                            <a href="index.html" class="tp-link">Analytical</a>
-                                        </li>
-                                        <li>
-                                            <a href="ecommerce.html" class="tp-link">E-commerce</a>
-                                        </li>
-                                    </ul>
-                                </div>
                             </li>
-
                             <!-- <li>
                                 <a href="landing.html" target="_blank">
                                     <i data-feather="globe"></i>
