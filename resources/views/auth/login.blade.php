0022411981f405cddf75e4fc17525355bd845866
Subrata Ghosh Dastidar <github.subrata@gmail.com>
2025-06-08 01:45:54 +0530

    add signup and forgot password link in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -42,14 +42,29 @@
                                         @csrf
                                         <div class="form-group mb-3">
                                             <label for="email" class="form-label">Email address</label>
-                                            <input class="form-control" type="email" id="email" name="email" required=""
-                                                placeholder="Enter your email">
+                                            <input class="form-control" type="email" id="email" name="email"
+                                                required="" placeholder="Enter your email">
                                         </div>
 
                                         <div class="form-group mb-3">
                                             <label for="password" class="form-label">Password</label>
                                             <input class="form-control" type="password" required="" id="password"
                                                 name="password" placeholder="Enter your password">
+                                        </div>
+                                        <div class="form-group d-flex mb-3">
+                                            <div class="col-sm-6">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input" id="checkbox-signin"
+                                                        checked>
+                                                    <label class="form-check-label" for="checkbox-signin">Remember
+                                                        me</label>
+                                                </div>
+                                            </div>
+                                            <div class="col-sm-6 text-end">
+                                                <a class='text-muted fs-14'
+                                                    href='{{ route('password.request') }}'>Forgot
+                                                    password?</a>
+                                            </div>
                                         </div>
                                         <div class="form-group mb-0 row">
                                             <div class="col-12">
@@ -59,6 +74,10 @@
                                             </div>
                                         </div>
                                     </form>
+                                    <div class="text-center text-muted mb-4">
+                                        <p class="mb-0">Don't have an account ?<a class='text-primary ms-2 fw-medium'
+                                                href='{{ route('register') }}'>Sing up</a></p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
